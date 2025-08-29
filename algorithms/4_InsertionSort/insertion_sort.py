86e20277322b5f72ae4706844a7cd96b26df66bb
shivansh0812 <shivanshsharma0812@gmail.com>
2025-08-29 09:43:30 +0530

    Update insertion_sort.py

--- algorithms/4_InsertionSort/insertion_sort.py
+++ algorithms/4_InsertionSort/insertion_sort.py
@@ -3,10 +3,10 @@ def insertion_sort(elements):
     for i in range(1, len(elements)):
         anchor = elements[i]
         j = i - 1
-        while j>=0 and anchor < elements[j]:
-            elements[j+1] = elements[j]
-            j = j - 1
-        elements[j+1] = anchor
+        while j>=0 and anchor < elements[j]:  #We are not past the beginning (j >= 0), and anchor is smaller than the element at index j.
+            elements[j+1] = elements[j]   # Shift the element at j one step to the right
+            j = j - 1  # Decrement j to check the previous element. This continues until we find the correct spot for anchor.
+        elements[j+1] = anchor  # When the loop ends, j will be one position left of where anchor b
 
 if __name__ == '__main__':
     elements = [11,9,29,7,2,15,28]
